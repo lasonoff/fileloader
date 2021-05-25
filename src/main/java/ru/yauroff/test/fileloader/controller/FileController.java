bd04a4a8ff5b1a24343b6e98ce913b3fbe5d2630
lasonoff <lason-vrn@yandex.ru>
2021-05-25 21:44:51 +0300

    Добавил Procfile для запуска приложения

--- src/main/java/ru/yauroff/test/fileloader/controller/FileController.java
+++ src/main/java/ru/yauroff/test/fileloader/controller/FileController.java
@@ -2,32 +2,73 @@ package ru.yauroff.test.fileloader.controller;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.yauroff.test.fileloader.service.impl.UserService;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class FileController extends HttpServlet {
-    private static final Logger logger = LogManager.getLogger(FileController.class);
+    private static final Logger logger = LogManager.getLogger(UsersController.class);
+    private static UserService userService;
+    //private static FileService fileService;
 
-    @Override
-    public void init() throws ServletException {
-        logger.info("Into FileController.init");
-    }
+    static final int fileMaxSize = 100 * 1024;
+    static final int memMaxSize = 100 * 1024;
 
-    @Override
+    private String filePath = "/home/proselyte/Programming/Projects/SerlvetsTutorial/src/main/resources/uploads/";
+    //private File file;
+
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
 
-        PrintWriter messageWriter = response.getWriter();
-        messageWriter.println("FileController");
-    }
+        String docType = "<!DOCTYPE html>";
+        String title = "File Uploading Demo";
 
-    @Override
-    public void destroy() {
-        logger.info("Into FileController.destroy");
+        /*PrintWriter writer = response.getWriter();
+
+
+        DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+        diskFileItemFactory.setRepository(new File(filePath));
+        diskFileItemFactory.setSizeThreshold(memMaxSize);
+
+        ServletFileUpload upload = new ServletFileUpload(diskFileItemFactory);
+        upload.setSizeMax(fileMaxSize);
+
+        try {
+            List fileItems = upload.parseRequest(request);
+
+            Iterator iterator = fileItems.iterator();
+
+            writer.println(docType +
+                    "<html>" +
+                    "<head>" +
+                    "<title>" + title + "</title>" +
+                    "</head>" +
+                    "<body>");
+
+            while (iterator.hasNext()) {
+                FileItem fileItem = (FileItem) iterator.next();
+                if (!fileItem.isFormField()) {
+
+                    String fileName = fileItem.getName();
+                    if (fileName.lastIndexOf("\\") >= 0) {
+                        file = new File(filePath +
+                                fileName.substring(fileName.lastIndexOf("\\")));
+                    } else {
+                        file = new File(filePath +
+                                fileName.substring(fileName.lastIndexOf("\\") + 1));
+                    }
+                    fileItem.write(file);
+                    writer.println(fileName + " is uploaded.<br>");
+                }
+            }
+            writer.println("</body>" +
+                    "</html>");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
     }
 }
